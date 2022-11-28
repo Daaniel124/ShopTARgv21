dc0e7d7756a38adfeafcb1af3d241ce0b81abdfa
daaniel124 <daaniel124@gmail.com>
2022-11-28 11:35:40 +0200

    SpaceshipControler

--- ShopTARgv21/ShopTARgv21/Controllers/SpaceshipController.cs
+++ ShopTARgv21/ShopTARgv21/Controllers/SpaceshipController.cs
@@ -1,11 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShopTARgv21.Data;
+using ShopTARgv21.Models.Spaceship;
 
 namespace ShopTARgv21.Controllers
 {
     public class SpaceshipController : Controller
     {
+        private readonly ShopDbContext _context;
+
+        public SpaceshipController
+            (
+                ShopDbContext context
+            )
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
+            var result = _context.Spaceship
+                .OrderByDescending(y => y.CreatedAt)
+                .Select(x => new SpaceshipListViewModel
+                {
+                    Name = x.Name,
+                    ModelType = x.ModelType,
+                    BuildOfDate = x.BuildOfDate,
+                    LaunchDate = x.LaunchDate
+                });
+
             return View();
         }
     }
