72ead45cdb57853cfb04aa0e2a5e0140fe72b415
daaniel124 <daaniel124@gmail.com>
2023-02-20 11:38:27 +0200

    WeatherForecast controller, viewmodel, services

---
+++ Shop/Shop/Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Models.WeatherForecast;
+using ShopTARgv21.ApplicationServices.Services;
+using ShopTARgv21.Core.ServiceInterface;
+
+namespace Shop.Controllers
+{
+    public class WeatherForecastController : Controller
+    {
+        private readonly IWeatherForecastServices _weatherForecastServices;
+
+        public WeatherForecastController
+            (
+             IWeatherForecastServices weatherForecastServices
+            )
+        {
+            _weatherForecastServices = weatherForecastServices;
+        }
+
+        [HttpGet]
+        public IActionResult SearchCity()
+        {
+            SearchCityViewModel vm = new();
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SearchCity(SearchCityViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("City", "WeatherForecast", new { city = vm.CityName });
+            }
+
+            return View(vm);
+        }
+    }
+}
---
+++ Shop/Shop/Models/WeatherForecast/SearchCityViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Models.WeatherForecast
+{
+    public class SearchCityViewModel
+    {
+        [Required(ErrorMessage = "You must enter a city name")]
+        [RegularExpression("^[A-Za-z]+$", ErrorMessage = "Only text allowed")]
+        [Display(Name = "City name ")]
+        public string CityName { get; set; }
+    }
+}
